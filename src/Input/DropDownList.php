bb19cddc01a0aa7b33785020391535ee80018230
Konstantin Voloshchuk <kosv.dev@gmail.com>
2018-11-07 23:47:48 +0600

    Release dropdownlist class

--- src/Input/DropDownList.php
+++ src/Input/DropDownList.php
@@ -7,11 +7,44 @@
 
 namespace Kosv\Yii2Grid\RowEditor\Input;
 
+use yii\helpers\Html;
+
 /**
  * @author Konstantin Voloshchuk <kosv.dev@gmail.com>
  * @since 1.0
  */
-class DropDownList implements InputInterface
+class DropDownList extends AbstractInput
 {
+    /**
+     * @var array
+     */
+    public $items = [];
 
+    /**
+     * @var string|array|null
+     */
+    public $selected = null;
+
+    /**
+     * @var string|array|null
+     */
+    public function getSelected()
+    {
+        return $this->selected !== null
+            ? $this->selected
+            : $this->getValue();
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return Html::dropDownList(
+            $this->getName(),
+            $this->getSelected(),
+            $this->items,
+            $this->options
+        );
+    }
 }
